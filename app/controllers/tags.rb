a0b98ee0710ff00aa9293fb9bac0ba1c327014a7
Jonathan Warshaw & Matthew Galloway <pair+jwarshaw.mwgalloway@devbootcamp.com>
2016-10-21 11:19:13 -0500

    Add entries/:id/tags post route

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,3 +1,1 @@
-post '/tags' do
 
-end
--- app/models/gist_tag.rb
+++ app/models/gist_tag.rb
@@ -3,5 +3,5 @@ class GistTag < ActiveRecord::Base
   belongs_to :tag
 
   validates_presence_of :gist_id, :tag_id
-  validates_uniqueness_of :gist_id, scope: :tag_idgist
+  validates_uniqueness_of :gist_id, scope: :tag_id
 end
